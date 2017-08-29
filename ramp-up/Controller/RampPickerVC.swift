daf9250749ac6261889b0fbcdc95e4ac0ebf892e
kitiwat chanluthin <aurelia@kitiwats-MacBook-Pro.local>
2017-08-29 14:14:30 +0700

    done tutorial

--- ramp-up/Controller/RampPickerVC.swift
+++ ramp-up/Controller/RampPickerVC.swift
@@ -32,6 +32,8 @@ class RampPickerVC: UIViewController {
         view.insertSubview(sceneView, at: 0)
         preferredContentSize = size
         
+        view.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        view.layer.borderWidth = 3.0
         let scene = SCNScene(named:"art.scnassets/ramps.scn")!
         sceneView.scene = scene
         
@@ -42,28 +44,17 @@ class RampPickerVC: UIViewController {
         let tap = UITapGestureRecognizer(target: self, action: #selector(handlerTap(_:)))
         sceneView.addGestureRecognizer(tap)
         
-        let rotate = SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: CGFloat(0.01 * Double.pi), z: 0, duration: 0.1))
+        let pipe = Ramp.getPipe()
+        Ramp.startRotation(node: pipe)
+        scene.rootNode.addChildNode(pipe)
         
-        var obj = SCNScene(named:"art.scnassets/pipe.dae")
-        var node = obj?.rootNode.childNode(withName:"pipe",recursively:true)!
-        node?.runAction(rotate!)
-        node?.scale = SCNVector3Make(0.0022, 0.0022, 0.0022)
-        node?.position = SCNVector3Make(-1, 0.7, -1)
-        scene.rootNode.addChildNode(node!)
+        let pyramid = Ramp.getPyramid()
+        Ramp.startRotation(node: pyramid)
+        scene.rootNode.addChildNode(pyramid)
         
-        obj = SCNScene(named: "art.scnassets/pyramid.dae")
-        node = obj?.rootNode.childNode(withName: "pyramid", recursively: true)!
-        node?.runAction(rotate!)
-        node?.scale = SCNVector3Make(0.0058, 0.0058, 0.0058)
-        node?.position = SCNVector3Make(-1, -0.45, -1)
-        scene.rootNode.addChildNode(node!)
-        
-        obj = SCNScene(named: "art.scnassets/quarter.dae")
-        node = obj?.rootNode.childNode(withName: "quarter", recursively: true)!
-        node?.runAction(rotate!)
-        node?.scale = SCNVector3Make(0.0058, 0.0058, 0.0058)
-        node?.position = SCNVector3Make(-1, -2.2, -1)
-        scene.rootNode.addChildNode(node!)
+        let quarter = Ramp.getQuarter()
+        Ramp.startRotation(node: quarter)
+        scene.rootNode.addChildNode(quarter)
     }
     
     @objc func handlerTap(_ gesture:UIGestureRecognizer){
@@ -72,8 +63,8 @@ class RampPickerVC: UIViewController {
         
         if hitResult.count>0{
             let node = hitResult[0].node
-            print("name : \(node.name)")
             rampPlacerVC.onRampSelected(rampName: node.name!)
+            dismiss(animated: true, completion: nil)
         }
     }
     
--- ramp-up/Controller/RampPlacerVC.swift
+++ ramp-up/Controller/RampPlacerVC.swift
@@ -12,7 +12,14 @@ import ARKit
 
 class RampPlacerVC: UIViewController, ARSCNViewDelegate,UIPopoverPresentationControllerDelegate {
 
+    @IBOutlet weak var control: UIStackView!
     @IBOutlet var sceneView: ARSCNView!
+    var selectedRampName:String?
+    @IBOutlet weak var rotateBtn: UIButton!
+    @IBOutlet weak var downBtn: UIButton!
+    @IBOutlet weak var upBtn: UIButton!
+    var selectedRamp:SCNNode?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,10 +31,24 @@ class RampPlacerVC: UIViewController, ARSCNViewDelegate,UIPopoverPresentationCon
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/pipe.dae")!
+        let scene = SCNScene(named: "art.scnassets/main.scn")!
+        sceneView.automaticallyUpdatesLighting = true
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        let gesture1 = UILongPressGestureRecognizer(target: self, action: #selector(onLongPressed(gesture:)))
+        let gesture2 = UILongPressGestureRecognizer(target: self, action: #selector(onLongPressed(gesture:)))
+        let gesture3 = UILongPressGestureRecognizer(target: self, action: #selector(onLongPressed(gesture:)))
+        
+        gesture1.minimumPressDuration = 0.1
+        gesture2.minimumPressDuration = 0.1
+        gesture3.minimumPressDuration = 0.1
+        
+        rotateBtn.addGestureRecognizer(gesture1)
+        upBtn.addGestureRecognizer(gesture2)
+        downBtn.addGestureRecognizer(gesture3)
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -78,6 +99,18 @@ class RampPlacerVC: UIViewController, ARSCNViewDelegate,UIPopoverPresentationCon
         
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard  let touch = touches.first else { return }
+        
+        let results = sceneView.hitTest(touch.location(in: sceneView), types: [.featurePoint])
+        
+        guard let hitFeature = results.last else { return }
+        
+        let hitTransform = SCNMatrix4(hitFeature.worldTransform)
+        let hitPositon = SCNVector3Make(hitTransform.m41, hitTransform.m42, hitTransform.m43)
+        placeRamp(position: hitPositon)
+    }
+    
     func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
         return .none
     }
@@ -95,8 +128,45 @@ class RampPlacerVC: UIViewController, ARSCNViewDelegate,UIPopoverPresentationCon
     }
     
     func onRampSelected(rampName:String){
+        selectedRampName = rampName
+    }
+    
+    func placeRamp(position:SCNVector3){
+        if let rampName = selectedRampName{
+            control.isHidden = false
+            let ramp = Ramp.getRampForName(rampName: rampName)
+            selectedRamp = ramp
+            ramp.position = position
+            ramp.scale = SCNVector3Make(0.01, 0.01, 0.01)
+            sceneView.scene.rootNode.addChildNode(ramp)
+        }
         
     }
     
+    @IBAction func removeBtnPressed(_ sender: Any) {
+        if let ramp = selectedRamp{
+            ramp.removeFromParentNode()
+            selectedRamp = nil
+        }
+    }
+    
+    @objc func onLongPressed(gesture:UILongPressGestureRecognizer){
+        if let ramp = selectedRamp{
+            if gesture.state == .ended{
+                ramp.removeAllActions()
+            }else if gesture.state == .began{
+                if gesture.view === rotateBtn{
+                    let rotate = SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: CGFloat(0.08 * Double.pi), z: 0, duration: 0.1))
+                    ramp.runAction(rotate!)
+                }else if gesture.view === upBtn{
+                    let move = SCNAction.repeatForever(SCNAction.moveBy(x: 0, y: 0.08, z: 0, duration: 0.1))
+                    ramp.runAction(move!)
+                }else if gesture.view === downBtn{
+                    let move = SCNAction.repeatForever(SCNAction.moveBy(x: 0, y: -0.08, z: 0, duration: 0.1))
+                    ramp.runAction(move!)
+                }
+            }
+        }
+    }
     
 }
