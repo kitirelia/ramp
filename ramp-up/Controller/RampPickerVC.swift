fb7b40ee10ce9bdfbe0f1443e2338bfc3f2b5af9
kitiwat chanluthin <aurelia@kitiwats-MacBook-Pro.local>
2017-08-29 10:20:06 +0700

    add rampickerVC and create popup

---
+++ ramp-up/Controller/RampPickerVC.swift
@@ -0,0 +1,39 @@
+//
+//  RampPickerVC.swift
+//  ramp-up
+//
+//  Created by kitiwat chanluthin on 8/29/17.
+//  Copyright © 2017 kitiwat chanluthin. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class RampPickerVC: UIViewController {
+
+    var sceneView:SCNView!
+    var size:CGSize!
+    
+    init(size:CGSize){
+        super.init(nibName:nil,bundle:nil)
+        self.size = size
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.frame = CGRect(origin: CGPoint.zero, size: size)
+        sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        view.insertSubview(sceneView, at: 0)
+        
+        preferredContentSize = size
+    }
+    
+    
+
+    
+}
